c06d0821339cc64366f9a1e8b9454cddd9ee30d3
sapinodejs@gmail.com <>
2019-12-11 13:55:51 +0100

    finish add dataModel into database

---
+++ src/main/java/unklick/Rezeptbuch/controllers/IndexController.java
@@ -0,0 +1,24 @@
+package unklick.Rezeptbuch.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import unklick.Rezeptbuch.services.RecipeService;
+
+@Controller
+public class IndexController {
+
+    private final RecipeService recipeService;
+
+    public IndexController(RecipeService recipeService) {
+        this.recipeService = recipeService;
+    }
+
+    @RequestMapping({"", "/", "/index"})
+    public String getIndexPage(Model model) {
+
+        model.addAttribute("recipes", recipeService.getRecipes());
+
+        return "index";
+    }
+}
--- src/main/java/unklick/Rezeptbuch/repositories/UnitOfMeasureRepository.java
+++ src/main/java/unklick/Rezeptbuch/repositories/UnitOfMeasureRepository.java
@@ -3,5 +3,8 @@ package unklick.Rezeptbuch.repositories;
 import org.springframework.data.repository.CrudRepository;
 import unklick.Rezeptbuch.domain.UnitOfMeasure;
 
+import java.util.Optional;
+
 public interface UnitOfMeasureRepository extends CrudRepository<UnitOfMeasure, Long> {
+    Optional<UnitOfMeasure> findByDescription(String description);
 }
