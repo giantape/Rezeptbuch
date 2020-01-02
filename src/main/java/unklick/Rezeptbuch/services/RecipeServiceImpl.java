d44c2e9523ef464d6d6aeaeffcbea7fb63eb282c
sapinodejs@gmail.com <>
2020-01-02 23:46:11 +0100

    add test

--- src/main/java/unklick/Rezeptbuch/services/RecipeServiceImpl.java
+++ src/main/java/unklick/Rezeptbuch/services/RecipeServiceImpl.java
@@ -1,12 +1,15 @@
 package unklick.Rezeptbuch.services;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import unklick.Rezeptbuch.domain.Recipe;
 import unklick.Rezeptbuch.repositories.RecipeRepository;
 
 import java.util.HashSet;
+import java.util.Optional;
 import java.util.Set;
 
+@Slf4j
 @Service
 public class RecipeServiceImpl implements RecipeService {
     private final RecipeRepository recipeRepository;
@@ -20,5 +23,14 @@ public class RecipeServiceImpl implements RecipeService {
         Set<Recipe> recipeSet = new HashSet<>();
         recipeRepository.findAll().iterator().forEachRemaining(recipeSet::add);
         return recipeSet;
+    }
+
+    @Override
+    public Recipe findById(Long l){
+        Optional<Recipe> recipeOptional = recipeRepository.findById(l);
+        if(!recipeOptional.isPresent()){
+            throw new RuntimeException("Recipe not found!");
+        }
+        return recipeOptional.get();
     }
 }
